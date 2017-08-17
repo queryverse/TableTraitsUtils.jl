4ca1dabbbed7ec3d1f4ac68903b2cc1827f4d3a5
David Anthoff <anthoff@berkeley.edu>
2017-08-17 13:03:52 -0700

    Add proper tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using TableTraitsImplementationHelpers
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "TableTraitsImplementationHelpers" begin
+
+columns = (Int[1,2,3], Float64[1.,2.,3.], String["John", "Sally", "Drew"])
+names = [:children, :age, :name]
+
+it = TableTraitsImplementationHelpers.create_tableiterator(columns, names)
+
+columns2, names2 = TableTraitsImplementationHelpers._fillcols(it)
+
+@test columns[1] == columns2[1]
+@test columns[2] == columns2[2]
+@test columns[3] == columns2[3]
+@test names == names2
+
+end
