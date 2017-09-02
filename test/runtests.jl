fa2f9429fd4d04d47b6f64d1517477f4739696af
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-09-02 17:20:14 +0100

    added test for sel_cols = all

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,16 +10,19 @@ it = TableTraitsUtils.create_tableiterator(columns, names)
 
 columns2, names2 = TableTraitsUtils.create_columns_from_iterabletable(it)
 
+columns3, names3 = TableTraitsUtils.create_columns_from_iterabletable(it, :all)
+
 columns23, names23 = TableTraitsUtils.create_columns_from_iterabletable(it, [2,3])
 
-@test columns[1] == columns2[1]
-@test columns[2] == columns2[2]
-@test columns[3] == columns2[3]
+@test columns[1] == columns2[1] == columns3[1]
+@test columns[2] == columns2[2] == columns3[2]
+@test columns[3] == columns2[3] == columns3[3]
+@test length(columns) == length(columns2) == length(columns3)
 @test columns[2] == columns23[1]
 @test columns[3] == columns23[2]
 @test length(columns23) == 2
 
-@test names == names2
+@test names == names2 == names3
 @test names[2:3] == names23
 
 end
