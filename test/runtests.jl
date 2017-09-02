32e51f1b903e664a0432aa5237a7c76b4a02042d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2017-09-02 17:13:10 +0100

    added test for sel_cols

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,9 +10,16 @@ it = TableTraitsUtils.create_tableiterator(columns, names)
 
 columns2, names2 = TableTraitsUtils.create_columns_from_iterabletable(it)
 
+columns23, names23 = TableTraitsUtils.create_columns_from_iterabletable(it, [2,3])
+
 @test columns[1] == columns2[1]
 @test columns[2] == columns2[2]
 @test columns[3] == columns2[3]
+@test columns[2] == columns23[1]
+@test columns[3] == columns23[2]
+@test length(columns23) == 2
+
 @test names == names2
+@test names[2:3] == names23
 
 end
