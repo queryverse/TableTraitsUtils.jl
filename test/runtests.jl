4ab4caaf607550058662bb809e37ad1b7d7fcd65
David Anthoff <anthoff@berkeley.edu>
2017-11-23 10:03:22 -0800

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using TableTraitsUtils
+using DataValues
 using Base.Test
 
 include("test_source_without_length.jl")
@@ -43,5 +44,26 @@ columns6, names6 = TableTraitsUtils.create_columns_from_iterabletable(it2, [2])
 @test columns6[1] == [1.,2.]
 @test names6 == [:b]
 
+columns_with_nulls = (Nullable{Int}[Nullable(3), Nullable(2), Nullable{Int}()], [2.,5.,9.], Nullable{String}[Nullable("a"), Nullable{String}(), Nullable("b")])
+it3 = TableTraitsUtils.create_tableiterator(columns_with_nulls, names)
+
+columns7, names7 = TableTraitsUtils.create_columns_from_iterabletable(it3)
+
+@test columns7[1] == [3,2,NA]
+@test columns7[2] == [2.,5.,9.]
+@test columns7[3] == ["a",NA,"b"]
+@test length(columns7) == 3
+@test names7 == names
+
+columns_with_DV = ([3, 2, NA], [2.,5.,9.], ["a", NA, "b"])
+it4 = TableTraitsUtils.create_tableiterator(columns_with_DV, names)
+
+columns8, names8 = TableTraitsUtils.create_columns_from_iterabletable(it4)
+
+@test columns8[1] == [3,2,NA]
+@test columns8[2] == [2.,5.,9.]
+@test columns8[3] == ["a",NA,"b"]
+@test length(columns8) == 3
+@test names8 == names
 
 end
